6388111650982b32d514886cc07fd1d7e6da4aee
Robert Elmore <Robertelmorebs2023@gmail.com>
2025-07-02 13:33:24 -0600

    api test update

--- tests/API.spec.ts
+++ tests/API.spec.ts
@@ -5,7 +5,18 @@ test('GET request to example API', async ({ request }) => {
   expect(response.ok()).toBeTruthy();
   const data = await response.json();
   expect(data).toHaveProperty('id', 1);
-  const response = await fetch('https://leagueify.org/api/data');
+
+});
+test('POST request to example API', async ({ request }) => {
+  const response = await request.post('https://leagueify.org/api/posts', {
+    data: { title: 'Test Post', body: 'This is a test post.' },
+  });
+  expect(response.ok()).toBeTruthy();
+  const data = await response.json();
+  expect(data).toHaveProperty('id');
+});
+test('get api data', async ({ request }) => {
+    const response = await fetch('https://leagueify.org/api/data');
 const data = await response.json();
 console.log(data);
 });
